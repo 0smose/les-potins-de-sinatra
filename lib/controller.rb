c05b16c27c5fdd724f4e02086dcaf8e581f5a592
Robin Dercole <robin.dercole@gmail.com>
2019-04-22 19:25:40 +0200

    quand y'en marre, y'en a marre

---
+++ lib/controller.rb
@@ -0,0 +1,31 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+	attr_accessor :author, :content
+
+
+ get '/' do
+  erb :index, locals: {gossips: Gossip.all}
+end
+
+ get '/gossips/new/' do
+ 	erb :new_gossip
+ end
+
+get '/gossips/:id' do
+	erb :show
+end
+
+get '/gossips/id/edit/' do
+	erb :edit
+end
+
+
+post '/gossips/new/' do
+	Gossip.new(params["gossip_author"], params["gossip_content"]).save
+	redirect '/'
+  end
+
+
+end
+
